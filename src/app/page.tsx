37f6db3988cc23eb92d8e2c601ab3c897aa7de80
NateFalconer <nate.falconer@protonmail.com>
2025-03-08 01:26:30 -0500

    scaffold ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,27 @@
 import Link from "next/link";
 
+
+const mockUrls = [
+  "https://1wl0fclwg5.ufs.sh/f/DFrWgeEqteSvxhTl7ejXl9B42omgatHhWfc8IRQ3KVGpu5ve",
+  "https://1wl0fclwg5.ufs.sh/f/DFrWgeEqteSvnJGsfeHWIxk9MKDAlf28LmRuiz4qU5t7CwZc",
+  "https://1wl0fclwg5.ufs.sh/f/DFrWgeEqteSvwLtr8WUeg9L1ivOT372CPqojhzxybdlSJ6VG",
+];
+
+const mockImages = mockUrls.map((url, index) => ({
+  id: index + 1,
+  url,
+}))
+
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      Hi - gallery t3 tutorial in progress
+    <main className="">
+      <div className="flex flex-wrap gap-4">
+        {[...mockImages, ...mockImages, ...mockImages].map((image) => (
+          <div key={image.id} className="w-48">
+            <img src={image.url} />
+          </div>
+        ))}  
+      </div>
     </main>
   );
 }
